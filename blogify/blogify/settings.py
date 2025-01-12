deb766c809826f0cdb159a5a9982e75f66ecc3e7
Yomi Olalude <yomiolalude@Yomis-MacBook-Pro.local>
2025-01-12 15:41:59 +0100

    add url to allowed_hosts

--- blogify/blogify/settings.py
+++ blogify/blogify/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cg2$9@j61(bd!^-^j!x#rga$#goc6%rtar_k8j3e(#$aljpkng
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["yomiolalude.pythonanywhere.com"]
 
 
 # Application definition
